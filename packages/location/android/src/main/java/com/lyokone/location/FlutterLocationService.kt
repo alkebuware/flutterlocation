a7dcbb810ab6e9ba050bebaca77c22a60ff74b2b
petersimonroadid <peter.simon@roadid.com>
2022-08-04 13:12:09 -0400

    Add check to address PendingIntent update

--- packages/location/android/src/main/java/com/lyokone/location/FlutterLocationService.kt
+++ packages/location/android/src/main/java/com/lyokone/location/FlutterLocationService.kt
@@ -10,6 +10,7 @@ import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import android.app.PendingIntent
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -53,7 +54,9 @@ class BackgroundNotification(
             ?.setPackage(null)
             ?.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
 
-        return if (intent != null) {
+        return if (intent != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_MUTABLE)
+        } else if (intent != null) {
             PendingIntent.getActivity(context, 0, intent, 0)
         } else {
             null
